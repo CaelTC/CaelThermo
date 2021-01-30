2b85d936cad18c364bbb75c5b3518cdae770e5b6
Cael <caeltrudeaucauchon@gmail.com>
2021-01-30 09:14:13 -0500

    e

--- thermo/TempButton2L.py
+++ thermo/TempButton2L.py
@@ -49,7 +49,7 @@ def chauffage():
 
 def cyclecounter():
     global test
-    if relay._active_state != relay._active_state :
+    if relay.is_active(True) != relay.is_active :
         test = test + 1
     print (int(test))    
 
